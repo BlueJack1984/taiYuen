0199bd18c75c4ea9e1f1ea551b652c3d8144e7a4
lushusheng <lushushen8@163.com>
2019-03-13 14:58:52 +0800

    first commit

---
+++ src/main/java/com/example/taiyuen/TaiyuenApplication.java
@@ -0,0 +1,13 @@
+package com.example.taiyuen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaiyuenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaiyuenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taiyuen/TaiyuenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.taiyuen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaiyuenApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
